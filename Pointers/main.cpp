02fa78d93e9daaca9a9fd7cc5625a9598fb7f21f
prasolova.nadya@mail.ru <prasolova.nadya@mail.ru>
2022-08-29 16:18:09 +0500

    Add project files.

---
+++ Pointers/main.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+//#define POINTERS_BASICS
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+#ifdef POINTERS_BASICS
+
+	int a = 2;				
+	int* pa = &a;			
+	
+	cout << a << endl;	
+	cout << &a << endl;	
+	cout << pa << endl;	
+	cout << *pa << endl;
+
+	int* pb;
+	int b = 3;
+	pb = &b;
+	// (int) (int*)
+		//int - int
+		//int* - указатель на int
+		//double - double
+		//double* - указатель на double
+	cout << b << endl;
+	cout << &b << endl;
+	cout << pb << endl;
+	cout << *pb << endl;
+
+#endif // POINTERS_BASICS
+
+	const int n = 5;
+	int arr[n] = { 3, 5, 8, 13, 21 };
+	cout << arr << endl;
+	cout << *arr << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << *(arr + i) << "\t";
+	}
+	cout << endl;
+}
+
