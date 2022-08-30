e81c58c8ae09260029da662e06b792971c528c00
prasolova.nadya@mail.ru <prasolova.nadya@mail.ru>
2022-08-30 23:20:26 +0500

    Reference

---
+++ References/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a = 2;
+	int& ra = a;	//reference to 'a' - ссылка на переменную 'a'
+	ra += 3;
+	cout << a << endl;
+	a += 3;
+	cout << ra << endl;
+	////////////////////////////////////
+	cout << &a << endl;
+	cout << &ra << endl;
+}
